70c9d0cf3468bfd49d05adfdab69fcd06592f551
Seval Cakir <sevalc@live.com>
2022-05-23 18:41:32 -0700

    basic files added/user model added

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim:true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+    },
+  {
+    toJSON: {
+        virtuals:true,
+        getters: true,
+    },
+    id:false
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
